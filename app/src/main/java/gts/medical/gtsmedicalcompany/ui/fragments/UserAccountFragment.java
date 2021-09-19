c2b0fd5d2ed90af2f7ee0a7c59c5037f262d9b27
eslam <eslammongy995@gmail.com>
2021-09-19 08:00:59 +0200

    update user profile image

--- app/src/main/java/gts/medical/gtsmedicalcompany/ui/fragments/UserAccountFragment.java
+++ app/src/main/java/gts/medical/gtsmedicalcompany/ui/fragments/UserAccountFragment.java
@@ -1,7 +1,7 @@
 package gts.medical.gtsmedicalcompany.ui.fragments;
 
 import android.Manifest;
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
@@ -12,6 +12,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
@@ -20,8 +24,6 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -31,7 +33,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
@@ -41,25 +42,49 @@ import java.util.Objects;
 import gts.medical.gtsmedicalcompany.constants.AppConstants;
 import gts.medical.gtsmedicalcompany.databinding.FragmentUserAccountBinding;
 import gts.medical.gtsmedicalcompany.model.UserModel;
-import gts.medical.gtsmedicalcompany.ui.activities.HomeActivity;
 import gts.medical.gtsmedicalcompany.utils.CustomDialog;
 import gts.medical.gtsmedicalcompany.utils.Util;
 
+@RequiresApi(api = Build.VERSION_CODES.R)
 public class UserAccountFragment extends Fragment {
 
     private FragmentUserAccountBinding binding;
     UserModel userModel;
     CustomDialog dialog;
 
+
+    private final ActivityResultContract cropActivityResultContract = new ActivityResultContract<Object, Object>() {
+        @NonNull
+        @Override
+        public Intent createIntent(@NonNull Context context, Object input) {
+            return CropImage.activity()
+                    .setAspectRatio(16, 9)
+                    .getIntent(requireContext());
+        }
+
+        @Override
+        public Object parseResult(int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent intent) {
+            return Objects.requireNonNull(CropImage.getActivityResult(intent)).getUri();
+        }
+    };
+    ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri) {
+                    uploadUserImage(uri);
+                }
+            });
+
     public UserAccountFragment() {
         // Required empty public constructor
     }
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-      binding = FragmentUserAccountBinding.inflate(inflater , container , false);
+        binding = FragmentUserAccountBinding.inflate(inflater, container, false);
 
-      return binding.getRoot();
+        return binding.getRoot();
     }
 
     @Override
@@ -68,71 +93,70 @@ public class UserAccountFragment extends Fragment {
         fetchUserInfo();
         dialog = new CustomDialog();
         binding.btnShowingUpdateDialog.setOnClickListener(v -> {
-                    binding.tvUserName.setText(userModel.getName());
-                    binding.tvUserAddress.setText(userModel.getAddress());
-                    binding.tvUserPhone.setText(userModel.getPhone());
-                    binding.tvUserEmail.setText(userModel.getEmail());
+            binding.tvUserName.setText(userModel.getName());
+            binding.tvUserAddress.setText(userModel.getAddress());
+            binding.tvUserPhone.setText(userModel.getPhone());
+            binding.tvUserEmail.setText(userModel.getEmail());
 
-          UpdateUserInfo updateUserInfo = new UpdateUserInfo(userModel.getName() , userModel.getEmail(), userModel.getPhone() , userModel.getAddress());
-          updateUserInfo.show(requireActivity().getSupportFragmentManager() , "TAG");
+            UpdateUserInfo updateUserInfo = new UpdateUserInfo(userModel.getName(), userModel.getEmail(), userModel.getPhone(), userModel.getAddress());
+            updateUserInfo.show(requireActivity().getSupportFragmentManager(), "TAG");
         });
 
         binding.changeUserProfilePhoto.setOnClickListener(v -> {
             if (checkUserStoragePermission()) {
-                pickUserImage();
+                mGetContent.launch("image/*");
             } else {
-                requestStoragePermission();
+                requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
             }
         });
 
     }
 
-    private void fetchUserInfo(){
-       binding.profileProgressBar.setVisibility(View.VISIBLE);
-       DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(Util.getUserID());
-                databaseReference.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if (snapshot.exists()){
-                            userModel = snapshot.getValue(UserModel.class);
-                            assert userModel != null;
-                            Glide.with(requireContext()).asBitmap().load(userModel.getImage()).into(binding.userProfilePhoto);
-                            binding.tvUserName.setText(userModel.getName());
-                            binding.tvUserAddress.setText(userModel.getAddress());
-                            binding.tvUserPhone.setText(userModel.getPhone());
-                            binding.tvUserEmail.setText(userModel.getEmail());
+    private void fetchUserInfo() {
+        binding.profileProgressBar.setVisibility(View.VISIBLE);
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(Util.getUserID());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    userModel = snapshot.getValue(UserModel.class);
+                    assert userModel != null;
+                    Glide.with(requireContext()).asBitmap().load(userModel.getImage()).into(binding.userProfilePhoto);
+                    binding.tvUserName.setText(userModel.getName());
+                    binding.tvUserAddress.setText(userModel.getAddress());
+                    binding.tvUserPhone.setText(userModel.getPhone());
+                    binding.tvUserEmail.setText(userModel.getEmail());
 
-                            binding.profileProgressBar.setVisibility(View.GONE);
-                        }
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
+                    binding.profileProgressBar.setVisibility(View.GONE);
+                }
+            }
 
-                    }
-                });
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.R)
-    private void updateUserImage(String imagePath){
+    private void updateUserImage(String imagePath) {
         DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(Util.getUserID());
-        Map<String, Object> map = Map.of("image",imagePath);
+        Map<String, Object> map = Map.of("image", imagePath);
 
         databaseReference.updateChildren(map);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.R)
-    private void uploadUserImage(Uri imageUri){
-        StorageReference storageReference= FirebaseStorage.getInstance().getReference();
-        storageReference.child(Util.getUserID() + AppConstants.ProfileImagePath).putFile(imageUri)
+    private void uploadUserImage(Uri imageUri) {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+        storageReference.child(FirebaseAuth.getInstance().getUid() + AppConstants.ProfileImagePath).putFile(imageUri)
                 .addOnSuccessListener(taskSnapshot -> {
                     Task<Uri> task = taskSnapshot.getStorage().getDownloadUrl();
                     task.addOnCompleteListener(uriTask -> {
-                        if (uriTask.isSuccessful()){
-                            String imagePath =uriTask.getResult().toString();
+                        if (uriTask.isSuccessful()) {
+                            String imagePath = Objects.requireNonNull(uriTask.getResult()).toString();
                             updateUserImage(imagePath);
-                        }else {
-                            Toast.makeText(requireActivity(), uriTask.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(requireActivity(), Objects.requireNonNull(uriTask.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     });
                 });
@@ -142,23 +166,14 @@ public class UserAccountFragment extends Fragment {
         return ContextCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.R)
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == Activity.RESULT_OK) {
-                 Uri imageUri = result.getUri();
-                Glide.with(this).asBitmap().load(imageUri).into(binding.userProfilePhoto);
-                uploadUserImage(imageUri);
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
-
-                System.out.print(error);
-            }
-        }
-    }
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    mGetContent.launch("image/*");
+                } else {
+                    requestStoragePermission();
+                }
+            });
 
     private void requestStoragePermission() {
         String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
@@ -167,15 +182,7 @@ public class UserAccountFragment extends Fragment {
                 , 1000
         );
     }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1000) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                pickUserImage();
-            else Toast.makeText(requireActivity(), "Storage permission denied", Toast.LENGTH_SHORT).show();
-        }
-    }
+
 
     private void pickUserImage() {
         CropImage.activity()
